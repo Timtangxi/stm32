d27256d475016eae2c95bf82d5f799e52de3cbb3
Timtangxi <3237125374@qq.com>
2024-06-07 18:40:30 +0800

    h750

--- f407_run.sh
+++ f407_run.sh
@@ -1,3 +1,6 @@
 cargo build --target thumbv7em-none-eabihf
-cargo build
-cargo build --example hello
+cargo build --target thumbv7em-none-eabihf --example hello
+cargo run --target thumbv7em-none-eabihf --example hello
+# openocd -f interface/stlink-v2.cfg -f target/stm32h7x.cfg
+# gdb-multiarch -q target/thumbv7em-none-eabihf/debug/examples/hello
+# monitor arm semihosting enable
